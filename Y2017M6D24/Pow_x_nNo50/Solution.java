bf89aeeda77a7237f634d64db7e5af708acd4e07
Allan Wong <allanwong94@163.com>
2017-06-24 08:42:09 +0800

    Leetcode Process: 81/551

---
+++ Y2017M6D24/Pow_x_nNo50/Solution.java
@@ -0,0 +1,43 @@
+package Y2017M6D24.Pow_x_nNo50;
+
+import java.util.HashMap;
+
+/**
+ * Created by Allan Wong on 2017/6/24.
+ */
+public class Solution {
+    //Runtime:20ms=>19ms Better than or equals to 88%=>97%
+    public double myPow(double x, int n) {
+        if(x==-1)
+            return n%2==0?1:-1;
+        if(x==1)
+            return 1;
+        double cur=x,res=1;
+        while(n>0){
+            if((n&1)==1){
+                res*=cur;
+            }
+            cur*=cur;
+            n=n>>1;
+        }
+        if(n<0)
+            return 1/myPow(x,-n);//Improvements
+        return res;
+    }
+
+    //Runtime:
+    /*
+    */
+
+
+
+
+    public static void main(String[] args) {
+        Solution s=new Solution();
+        //System.out.println(s.myPow(2,22));
+        System.out.println(Math.pow(2,-1100));
+        System.out.println("Done.");
+    }
+
+
+}
---
+++ Y2017M6D24/PowerofThree/Solution.java
@@ -0,0 +1,34 @@
+package Y2017M6D24.PowerofThree;
+
+/**
+ * Created by Allan Wong on 2017/6/24.
+ */
+public class Solution {
+    //Runtime:17ms Better than or equals to 91%
+    //Requirement not met: no loops/recursion
+    public boolean isPowerOfThree(int n) {
+        if(n==0)
+            return false;
+        if(n==1)
+            return true;
+        return n%3==0?isPowerOfThree(n/3):false;
+    }
+    //Runtime:
+    /*
+    public boolean isPowerOfThree(int n) {
+        // 1162261467 is 3^19,  3^20 is bigger than int
+        return ( n>0 &&  1162261467%n==0);
+    }
+    */
+
+
+
+
+    public static void main(String[] args) {
+        Solution s=new Solution();
+        System.out.println();
+        System.out.println("Done.");
+    }
+
+
+}
